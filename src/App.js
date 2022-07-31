499b00a1c20d986c79bf04499d841ab0fa4db0f5
methodDen <daniyarabsatov@gmail.com>
2022-07-31 16:20:27 +0600

    added change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Kek brek pek shpek
+          I am changed!
         </p>
         <a
           className="App-link"
